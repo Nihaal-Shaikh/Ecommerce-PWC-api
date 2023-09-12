975f0c92c77e55c9310d327973a62c728897dbda
Your Name <nihaal@sapnagroup.com>
2023-09-12 13:00:40 +0530

    Contact rest api created

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
+use App\Http\Controllers\Admin\ContactController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
+// Get Visitor
 Route::get('/getVisitor', [VisitorController::class, 'GetVisitorDetails']);
+
+// Contact Page
+Route::post('/postContact', [ContactController::class, 'PostContactDetails']);
